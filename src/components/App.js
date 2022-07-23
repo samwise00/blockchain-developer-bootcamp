032b381f62182bf28b0317a80c342c399ad27b3d
samwise00 <sam.sandoval.00@gmail.com>
2022-07-23 17:34:18 -0400

    18. Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ import {
   loadExchange
 } from '../store/interactions';
 
+import Navbar from './Navbar';
+
 function App() {
   const dispatch = useDispatch()
 
@@ -20,9 +22,16 @@ function App() {
     // Fetch current network's chainId(e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
-    // Fetch current account & balance from Metamask
-    await loadAccount(provider, dispatch)
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
 
+    // Fetch current account & balance from Metamask
+    window.ethereum.on('accountsChanged', async () => {
+      await loadAccount(provider, dispatch)
+    })
+    
     // Load token smart contracts
     const DApp = config[chainId].DApp
     const mETH = config[chainId].mETH
@@ -43,6 +52,7 @@ function App() {
     <div>
 
       {/* Navbar */}
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
