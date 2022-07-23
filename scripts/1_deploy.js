49214c7d0ac04147f640399a8d966b5f923a58f2
samwise00 <sam.sandoval.00@gmail.com>
2022-07-22 20:12:39 -0400

    15. UI Setup

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,31 @@
-async function main() {
-    // Fetch contract to deploy
-    const Token = await ethers.getContractFactory("Token");
+const { ethers } = require("hardhat");
 
-    // Deploy contract
-    const token = await Token.deploy();
-    await token.deployed()
-    console.log(`Token deployed to address: ${token.address}`)
+async function main() {
+    console.log(`Preparing deployment... \n`)
+    // Fetch contract to deploy
+    const Token = await ethers.getContractFactory('Token')
+    const Exchange = await ethers.getContractFactory('Exchange')
+
+    const accounts = await ethers.getSigners()
+    
+    console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`)
+
+    // Deploy contracts
+    const dapp = await Token.deploy('Dapp University', 'DAPP', '1000000');
+    await dapp.deployed()
+    console.log(`DAPP deployed to address: ${dapp.address}`)
+
+    const mETH = await Token.deploy('mETH', 'mETH', '1000000');
+    await mETH.deployed()
+    console.log(`mETH deployed to address: ${mETH.address}`)
+
+    const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000');
+    await mDAI.deployed()
+    console.log(`mDAI deployed to address: ${mDAI.address}`)
+
+    const exchange = await Exchange.deploy(accounts[1].address, 10)
+    await exchange.deployed()
+    console.log(`Exchange Deployed to: ${exchange.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
