f44c120b5c8e65a912d609a85bbc609c96212a11
samwise00 <sam.sandoval.00@gmail.com>
2022-07-25 01:14:12 -0400

    20. Deposits pt 1

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
   loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions';
 
 import Navbar from './Navbar';
 import Markets from './Markets';
+import Balance from './Balance';
 
 function App() {
   const dispatch = useDispatch()
@@ -41,8 +43,11 @@ function App() {
     await loadTokens(provider, [DApp.address, mETH.address], dispatch)
 
     // Load exchange Smart Contract
-    const exchange = config[chainId].exchange
-    await loadExchange(provider, exchange.address, dispatch)
+    const exchangeConfig = config[chainId].exchange
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+
+    // listen to events
+    subscribeToEvents(exchange, dispatch)
   }
 
   useEffect(() => {
@@ -62,6 +67,7 @@ function App() {
           <Markets />
 
           {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
