4325b97e360c64dd38bdda4c07f2179c88d5ca9b
samwise00 <sam.sandoval.00@gmail.com>
2022-07-26 11:02:52 -0400

    25. Price Chart

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ import Navbar from './Navbar'
 import Markets from './Markets'
 import Balance from './Balance'
 import Order from './Order'
+import PriceChart from './PriceChart'
 import OrderBook from './OrderBook'
 
 function App() {
@@ -76,6 +77,7 @@ function App() {
         <section className='exchange__section--right grid'>
 
           {/* PriceChart */}
+          <PriceChart />
 
           {/* Transactions */}
 
