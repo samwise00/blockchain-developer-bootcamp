1c9b083d144e1033f8143cdc1e787fa88adc52f1
samwise00 <sam.sandoval.00@gmail.com>
2022-07-29 13:25:10 -0400

    add rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {}
+    localhost: {},
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.RINKEBY_API_KEY}`,
+      accounts: privateKeys.split(',')
+    },
   },
 };
